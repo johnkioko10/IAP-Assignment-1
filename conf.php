1987bac5637e7798cbaf0e2530668b14601b76a4
johnkioko10 <john.musyimi@strathmore.edu>
2025-09-14 22:26:31 +0300

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,26 @@
+<?php
+// Site timezone
+$conf['site_timezone'] = 'AFRICA/NAIROBI';
+
+// Site information
+$conf['site_name'] = 'ICS Academy';
+$conf['site_url'] = 'http://localhost/fol/';
+$conf['site_email'] = 'info@icsacademy.com';
+
+// Site language
+$conf['site_lang'] = 'en';
+
+// Database Constants
+$conf['DB_TYPE'] = 'mysqli';
+$conf['DB_HOST'] = 'localhost';
+$conf['DB_USER'] = 'root';
+$conf['DB_PASS'] = 'mosesmugo';
+$conf['DB_NAME'] = 'fol';
+
+// Email configuration
+$conf['mail_type'] = 'smtp'; // mail or smtp
+$conf['smtp_host'] = 'smtp.gmail.com'; // SMTP Host Address
+$conf['smtp_user'] = 'mugomoses506@gmail.com'; // SMTP Username
+$conf['smtp_pass'] = 'ywjjcupuqfneuttm'; // SMTP Password
+$conf['smtp_port'] = 465; // SMTP Port - 587 for tls, 465 for ssl
+$conf['smtp_secure'] = 'ssl'; // Encryption - ssl or tls
